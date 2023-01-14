b80d7e65614ba7eaec2d1fdd1af9d27bb8c02733
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-01-14 15:22:48 +0530

    Added Button Ripple Effect project

---
+++ Button Ripple Effect/script.js
@@ -0,0 +1,8 @@
+const btnEl = document.getElementById("btn");
+
+btnEl.addEventListener("mouseover", (e) => {
+	const x = e.pageX - btnEl.offsetLeft;
+	const y = e.pageY - btnEl.offsetTop;
+	btnEl.style.setProperty("--xPos", x + "px");
+	btnEl.style.setProperty("--yPos", y + "px");
+});
