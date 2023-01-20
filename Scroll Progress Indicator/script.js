72e7a1f5507c914a505a18c001403a441409389a
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-01-20 22:30:17 +0530

    minor code refactoring

--- Scroll Progress Indicator/script.js
+++ Scroll Progress Indicator/script.js
@@ -1,7 +1,7 @@
 const progressBarEl = document.getElementById("progress-bar");
 
 window.addEventListener("scroll", () => {
-	let height = parseInt(document.body.scrollHeight) - parseInt(window.innerHeight);
+	let height = document.body.scrollHeight - window.innerHeight;
 	let scrollPosition = document.documentElement.scrollTop;
 	let width = (scrollPosition / height) * 100;
 	progressBarEl.style.width = `${width}%`;
