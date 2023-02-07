a38d73cd49ab6119a88a2e3dd54382f940efce1e
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-02-07 19:00:48 +0530

    project 20 completed

--- 20. Text to Speech/script.js
+++ 20. Text to Speech/script.js
@@ -1,6 +1,37 @@
-const msg = new SpeechSynthesisUtterance();
-let voices = [];
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+	voices = this.getVoices();
+	const voiceOptions = voices
+		.filter((voice) => voice.lang.substring(0, 2) === "en")
+		.map((voice) => `<option value=${voice.name}>${voice.name} (${voice.lang})</option>`)
+		.join("");
+	voicesDropdown.innerHTML = voiceOptions;
+}
+
+function setVoice() {
+	speechSynthesis.cancel();
+	msg.voice = voices.find((voice) => voice.name === this.value);
+}
+
+function startSpeech() {
+	speechSynthesis.cancel();
+	speechSynthesis.speak(msg);
+}
+
+function setOptions() {
+	console.log(this.name, this.value);
+	msg[this.name] = this.value;
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+speakButton.addEventListener("click", startSpeech);
+options.forEach((option) => option.addEventListener("input", setOptions));
